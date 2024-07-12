c8e07de6464034cc6dbf0daca350222677148d89
Aditia A. Pratama <aditia.ap@gmail.com>
2024-07-13 00:55:03 +0800

    adjust mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,25 +3,25 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
-map("i", "jj", "<ESC>")
+-- map("i", "jk", "<ESC>")
+-- map("i", "jj", "<ESC>")
 -- map("n", "<leader>sv", ":source $MYVIMRC<CR>", { noremap = true, silent = true })
 -- Move
-local opts = { noremap = true, silent = true }
 -- Normal-mode commands
-vim.keymap.set('n', '<A-j>', ':MoveLine(1)<CR>', opts)
-vim.keymap.set('n', '<A-k>', ':MoveLine(-1)<CR>', opts)
--- vim.keymap.set('n', '<A-h>', ':MoveHChar(-1)<CR>', opts)
--- vim.keymap.set('n', '<A-l>', ':MoveHChar(1)<CR>', opts)
-vim.keymap.set('n', '<leader>wf', ':MoveWord(1)<CR>', opts)
-vim.keymap.set('n', '<leader>wb', ':MoveWord(-1)<CR>', opts)
+map('n', '<A-j>', ':MoveLine(1)<CR>', opts)
+map('n', '<A-k>', ':MoveLine(-1)<CR>', opts)
+-- map('n', '<A-h>', ':MoveHChar(-1)<CR>', opts)
+-- map('n', '<A-l>', ':MoveHChar(1)<CR>', opts)
+map('n', '<leader>wf', ':MoveWord(1)<CR>', opts)
+map('n', '<leader>wb', ':MoveWord(-1)<CR>', opts)
 
 -- Visual-mode commands
-vim.keymap.set('v', '<A-j>', ':MoveBlock(1)<CR>', opts)
-vim.keymap.set('v', '<A-k>', ':MoveBlock(-1)<CR>', opts)
-vim.keymap.set('v', '<A-h>', ':MoveHBlock(-1)<CR>', opts)
-vim.keymap.set('v', '<A-l>', ':MoveHBlock(1)<CR>', opts)
+map('v', '<A-j>', ':MoveBlock(1)<CR>', opts)
+map('v', '<A-k>', ':MoveBlock(-1)<CR>', opts)
+map('v', '<A-h>', ':MoveHBlock(-1)<CR>', opts)
+map('v', '<A-l>', ':MoveHBlock(1)<CR>', opts)
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
