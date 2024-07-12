abba3b2b8cf5f3e0fc0ac04c00576f3f674828d0
Aditia A. Pratama <aditia.ap@gmail.com>
2024-07-13 00:56:10 +0800

    houdini and outline config file

---
+++ lua/configs/houdini.lua
@@ -0,0 +1,3 @@
+local options = {}
+
+require("houdini").setup(options)
---
+++ lua/configs/outline.lua
@@ -0,0 +1,12 @@
+local options = {
+  outline_window = {
+    width = 10,
+  },
+  keymaps = {
+    peek_location = {
+      "<LeftRelease>",
+    },
+  },
+}
+
+require("outline").setup(options)
