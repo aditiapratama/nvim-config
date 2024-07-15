e1d2814b485e5c361ddb377521d8ec19de78f1dd
Aditia A. Pratama <aditia.ap@gmail.com>
2024-07-15 18:01:42 +0800

    add tmux plugin

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,28 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+--
+-- Kitty
+local autocmd = vim.api.nvim_create_autocmd
+local o = vim.o
+local uv = vim.loop
+
+autocmd("VimEnter", {
+  command = ":silent !kitty @ set-spacing padding=0 margin=0",
+})
+
+autocmd("VimLeavePre", {
+  command = ":silent !kitty @ set-spacing padding=20 margin=10",
+})
+
+autocmd("VimEnter", {
+  callback = function()
+    if vim.env.TMUX_PLUGIN_MANAGER_PATH then
+      uv.spawn(vim.env.TMUX_PLUGIN_MANAGER_PATH .. "tmux-window-name/scripts/rename_session_windows.py", {})
+    end
+  end,
+})
+
+o.foldmethod = "expr"
+o.foldexpr = "nvim_treesitter#foldexpr()"
+o.foldenable = false
