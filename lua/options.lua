609161a62e8833358484a5cb1f65fb5807b7a361
Aditia A. Pratama <aditia.ap@gmail.com>
2024-07-22 17:47:19 +0800

    update nvim config options

--- lua/options.lua
+++ lua/options.lua
@@ -5,18 +5,8 @@ require "nvchad.options"
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 --
--- Kitty
 local autocmd = vim.api.nvim_create_autocmd
-local o = vim.o
 local uv = vim.loop
-
-autocmd("VimEnter", {
-  command = ":silent !kitty @ set-spacing padding=0 margin=0",
-})
-
-autocmd("VimLeavePre", {
-  command = ":silent !kitty @ set-spacing padding=20 margin=10",
-})
 
 autocmd("VimEnter", {
   callback = function()
@@ -26,6 +16,50 @@ autocmd("VimEnter", {
   end,
 })
 
--- o.foldmethod = "expr"
--- o.foldexpr = "nvim_treesitter#foldexpr()"
--- o.foldenable = false
+local options = {
+  laststatus = 3, -- global statusline
+  backup = false, -- creates a backup file
+  conceallevel = 0, -- so that `` is visible in markdown files
+  fileencoding = "utf-8", -- the encoding written to a file
+  hidden = true, -- required to keep multiple buffers and open multiple buffers
+  ignorecase = true, -- ignore case in search patterns
+  mouse = "a", -- allow the mouse to be used in neovim
+  pumheight = 8, -- pop up menu height
+  pumblend = 0, -- transparency of pop-up menu
+  showmode = false, -- we don't need to see things like -- INSERT -- anymore
+  smartcase = true, -- smart case
+  smartindent = true, -- make indenting smarter again
+  splitbelow = true, -- force all horizontal splits to go below current window
+  splitright = true, -- force all vertical splits to go to the right of current window
+  swapfile = true, -- creates a swapfile
+  timeoutlen = 500, -- time to wait for a mapped sequence to complete (in milliseconds)
+  undofile = true, -- enable persistent undo
+  updatetime = 100, -- faster completion (4000ms default)
+  writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+  expandtab = true, -- convert tabs to spaces
+  shiftwidth = 2, -- the number of spaces inserted for each indentation
+  tabstop = 2, -- insert 2 spaces for a tab
+  cursorline = false, -- highlight the current line
+  number = true, -- set numbered lines
+  relativenumber = true, -- set relative numbered lines
+  numberwidth = 5, -- set number column width to 4 {default 4}
+  signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
+  wrap = false, -- display lines as one long line
+  scrolloff = 8, -- minimal number of columns to scroll horizontally.
+  sidescrolloff = 8, -- minimal number of screen columns
+  lazyredraw = false, -- Won't be redrawn while executing macros, register and other commands.
+  termguicolors = true, -- Enables 24-bit RGB color in the TUI
+  fillchars = { eob = " ", fold = " ", foldopen = "", foldsep = " ", foldclose = "", lastline = " " }, -- make EndOfBuffer invisible
+  ruler = false,
+  cmdheight = 0,
+  -- shell = vim.fn.executable "pwsh" and "pwsh" or "powershell",
+  -- shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+  -- shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+  -- shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+  -- shellquote = "",
+  -- shellxquote = "",
+}
+
+for name, value in pairs(options) do
+  vim.opt[name] = value
+end
