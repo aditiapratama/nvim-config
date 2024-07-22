5f76ab841d1bdd32b0c8675cecba9ac519ef586b
Aditia A. Pratama <aditia.ap@gmail.com>
2024-07-22 22:02:34 +0800

    fix outline view

--- lua/configs/outline.lua
+++ lua/configs/outline.lua
@@ -1,11 +1,17 @@
 local options = {
   outline_window = {
     width = 15,
+    -- wrap = true,
+    hide_cursor = true,
+    focus_on_open = false,
   },
   keymaps = {
     peek_location = {
       "<LeftRelease>",
     },
+  },
+  preview_window = {
+    open_hover_on_preview = true,
   },
 }
 
