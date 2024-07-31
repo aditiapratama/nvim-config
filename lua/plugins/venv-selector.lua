0b799e1e962632e5d006ec0db56f540d74b947af
Aditia A. Pratama <aditia.ap@gmail.com>
2024-08-01 00:12:42 +0800

    venv select will remember pyenv virtualenv setup

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -7,13 +7,14 @@ return {
     "mfussenegger/nvim-dap-python", --optional
     { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
   },
+  lazy = false,
   branch = "regexp",
-  cmd = { "VenvSelect", "VenvSelectCached" },
-  init = function()
-    vim.keymap.set("n", "<leader>vs", "<cmd>VenvSelect<cr>", { desc = "Venv Selector | Select Python venv" })
+  config = function()
+    require("venv-selector").setup {
+      pyenv_path = "/home/aditia/.pyenv/versions",
+    }
   end,
-  opts = {},
-  config = function(_, opts)
-    require("venv-selector").setup(opts)
-  end,
+  keys = {
+    { "<leader>vs", "<cmd>VenvSelect<cr>", desc = "Venv Selector | Select Python venv" },
+  },
 }
