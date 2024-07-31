212ef40af5dee05c432073ebe7a91fac72899a4c
Aditia A. Pratama <aditia.ap@gmail.com>
2024-07-31 19:42:53 +0800

    now mypy will respect virtualenv as well

--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -1,8 +1,14 @@
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local null_ls = require "null-ls"
 local options = {
   sources = {
     null_ls.builtins.formatting.black,
-    null_ls.builtins.diagnostics.mypy,
+    null_ls.builtins.diagnostics.mypy.with {
+      extra_args = function()
+        local virtual = os.getenv "VIRTUAL_ENV" or "/usr"
+        return { "--python-executable", virtual .. "/bin/python" }
+      end,
+    },
     require "none-ls.diagnostics.ruff",
   },
   on_attach = function(client, bufnr)
