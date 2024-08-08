d8542628a79ed0599a47b7f84590227bafbf82ed
Aditia A. Pratama <aditia.ap@gmail.com>
2024-08-09 00:48:51 +0800

    update none-ls plugin

--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -1,6 +1,8 @@
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local null_ls = require "null-ls"
 local options = {
+  debounce = 150,
+  -- save_after_format = false,
   sources = {
     null_ls.builtins.formatting.black,
     null_ls.builtins.diagnostics.mypy.with {
@@ -10,7 +12,24 @@ local options = {
       end,
     },
     require "none-ls.diagnostics.ruff",
+    -- null_ls.builtins.code_actions.cspell,
+    null_ls.builtins.code_actions.refactoring,
+    -- null_ls.builtins.diagnostics.cspell,
+    null_ls.builtins.diagnostics.fish,
+    null_ls.builtins.diagnostics.markdownlint,
+    null_ls.builtins.formatting.fish_indent,
+    null_ls.builtins.formatting.mdformat,
+    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.shfmt,
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.code_actions.gitsigns,
+    null_ls.builtins.diagnostics.selene.with {
+      condition = function(utils)
+        return utils.root_has_file { "selene.toml" }
+      end,
+    },
   },
+  root_dir = require("null-ls.utils").root_pattern("package.json", ".null-ls-root", ".neoconf.json", ".git"),
   on_attach = function(client, bufnr)
     if client.supports_method "textDocument/formatting" then
       vim.api.nvim_clear_autocmds {
