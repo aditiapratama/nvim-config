ee1e1f4d8dcd082afb04a8438d19405b0f07859c
Aditia A. Pratama <aditia.ap@gmail.com>
2024-08-09 00:06:02 +0800

    add quicker plugin

---
+++ lua/plugins/quicker.lua
@@ -0,0 +1,9 @@
+---@type NvPluginSpec
+-- NOTE: Better QuickFixList
+return {
+  "stevearc/quicker.nvim",
+  ---@module "quicker"
+  ---@type quicker.SetupOptions
+  opts = {},
+  event = "User FilePost",
+}
