a508794ebab5e65f30fe1352a9d11d7fad9f6896
Aditia A. Pratama <aditia.ap@gmail.com>
2024-08-08 10:59:19 +0800

    adjust outline plugin

--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -2,22 +2,33 @@
 return {
   "hedyhli/outline.nvim",
   lazy = true,
+  -- event = "User OutlineOpened",
   cmd = { "Outline", "OutlineOpen" },
   keys = { -- Example mapping to toggle outline
     { "<leader>ol", "<cmd>Outline<CR>", desc = "Toggle outline" },
   },
+  -- config = function()
+  --   require("outline").setup({})
+  -- end,
   opts = {
     -- Your setup opts here
-    preview_window = {
-      auto_preview = false,
-      winhl = 'NormalFloat:',
+    -- preview_window = {
+    --   auto_preview = false,
+    --   winhl = 'NormalFloat:',
+    -- },
+    outline_items = {
+      show_symbol_lineno = false,
+      show_symbol_details = false,
     },
     outline_window = {
+      -- position = "left",
       winhl = "",
       width = 15,
       show_cursor_line = true,
       hide_cursor = true,
       focus_on_open = false,
+      -- split_command = 'rightbelow 25vsp'
+      -- wrap = true,
     },
     keymaps = {
       peek_location = {
@@ -27,11 +38,11 @@ return {
     -- preview_window = {
     --   open_hover_on_preview = true,
     -- },
-    symbols = {
-      filter = {
-        default = { "String", "Variable", exclude = true },
-        python = { "Function", "Class" },
-      },
-    },
+    -- symbols = {
+    --   filter = {
+    --     default = { "String", "Variable", exclude = true },
+    --     python = { "Function", "Class" },
+    -- },
+    -- },
   },
 }
