882adba6f6ecd850b063ce9ba47631d8c45423e1
Aditia A. Pratama <aditia.ap@gmail.com>
2024-08-08 10:58:52 +0800

    nvimtree disable diagnostic and cursorline

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -63,6 +63,15 @@ return {
   "nvim-tree/nvim-tree.lua",
   opts = {
     on_attach = on_attach,
+    diagnostics = {
+      enable = false,
+      icons = {
+        hint = "󰌵",
+        info = "",
+        warning = "",
+        error = "",
+      },
+    },
     sync_root_with_cwd = true,
     update_focused_file = {
       enable = true,
@@ -77,7 +86,7 @@ return {
       timeout = 5000,
     },
     view = {
-      cursorline = true,
+      cursorline = false,
       float = {
         enable = false,
         quit_on_focus_loss = true,
@@ -93,8 +102,39 @@ return {
     },
     renderer = {
       highlight_git = false,
-      -- root_folder_label = false,
-      root_folder_label = ":~:s?$?",
+      root_folder_label = false,
+      -- root_folder_label = ":~:s?$?",
+      icons = {
+        show = {
+          file = true,
+          folder = true,
+          folder_arrow = true,
+          git = true,
+        },
+        glyphs = {
+          default = "󰈚",
+          symlink = "",
+          folder = {
+            default = "",
+            empty = "",
+            empty_open = "",
+            open = "",
+            symlink = "",
+            symlink_open = "",
+            arrow_open = "",
+            arrow_closed = "",
+          },
+          git = {
+            unstaged = "",
+            staged = "✓",
+            unmerged = "",
+            renamed = "➜",
+            untracked = "U",
+            deleted = "",
+            ignored = "◌",
+          },
+        },
+      },
     },
     filters = {
       dotfiles = false,
