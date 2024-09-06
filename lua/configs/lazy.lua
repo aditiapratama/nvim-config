e7d888faf683a477fdf57eea75323917f1de5a84
Aditia A. Pratama <aditia.ap@gmail.com>
2024-09-06 16:37:23 +0800

    add update checker

--- lua/configs/lazy.lua
+++ lua/configs/lazy.lua
@@ -1,8 +1,16 @@
 return {
   defaults = { lazy = true },
   install = { colorscheme = { "nvchad" } },
-
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+  change_detection = {
+    -- automatically check for config file changes and reload the ui
+    enabled = true,
+    notify = false, -- get a notification when changes are found
+  },
+  lockfile = vim.fn.stdpath "data" .. "/lazy-lock.json",
   ui = {
+    border = vim.g.border_enabled and "rounded" or "none",
     icons = {
       ft = "",
       lazy = "󰂠 ",
