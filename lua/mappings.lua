b8cae9276ad3e161e3da29319da65e9525e4325f
Aditia A. Pratama <aditia.ap@gmail.com>
2024-09-06 16:38:16 +0800

    update mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -45,3 +45,78 @@ map("n", "<leader>q", "<cmd>qa!<cr>", { desc = "General | Quit", silent = true }
 
 -- Close Buffer
 map("n", "<leader>c", "<cmd>Bdelete!<cr>", { desc = "General | Close Buffer", silent = true })
+
+-- Remove All Text 
+map("n", "<leader>R", "<cmd>%d+<cr>", { desc = "General | Remove All Text", silent = true })
+
+-- Yank all Text
+map("n", "<leader>y", "<cmd>%y+<cr>", { desc = "General | Yank All Text", silent = true })
+
+-- Better Down
+map("n", "j", "v:count == 0 ? 'gj' : 'j'", { desc = "General | Better Down", expr = true, silent = true })
+
+-- Better Up
+map("n", "k", "v:count == 0 ? 'gk' : 'k'", { desc = "General | Better Up", expr = true, silent = true })
+
+-- Better Down (Visual Mode)
+map("v", "j", "v:count == 0 ? 'gj' : 'j'", { desc = "General | Better Down", expr = true, silent = true })
+
+-- Better Up (Visual Mode)
+map("v", "k", "v:count == 0 ? 'gk' : 'k'", { desc = "General | Better Up", expr = true, silent = true })
+
+-- Indent backward
+map("n", "<", "<<", { desc = "General | Indent backward", silent = true })
+
+-- Indent forward
+map("n", ">", ">>", { desc = "General | Indent forward", silent = true })
+
+-- Indent backward (Visual Mode)
+map("v", "<", "<gv", { desc = "General | Indent backward", silent = true })
+
+-- Indent forward (Visual Mode)
+map("v", ">", ">gv", { desc = "General | Indent forward", silent = true })
+
+-- Better Paste (Visual Mode)
+map("v", "p", '"_dP', { desc = "General | Better Paste", silent = true })
+
+-- Messages
+map("n", "<leader>nm", "<cmd>messages<cr>", { desc = "Neovim | Messages", silent = true })
+
+-- Health
+map("n", "<leader>nh", "<cmd>checkhealth<cr>", { desc = "Neovim | Health", silent = true })
+
+-- Version
+map("n", "<leader>nv", function()
+  local version = vim.version().major .. "." .. vim.version().minor .. "." .. vim.version().patch
+  return vim.notify(version, vim.log.levels.INFO, { title = "Neovim Version" })
+end, { desc = "Neovim | Version", silent = true })
+
+-- Clean
+vim.keymap.set("n", "<leader>pc", "<cmd>Lazy clean<cr>", { desc = "Lazy | Clean", silent = true })
+
+-- Check
+vim.keymap.set("n", "<leader>pC", "<cmd>Lazy check<cr>", { desc = "Lazy | Check", silent = true })
+
+-- Debug
+vim.keymap.set("n", "<leader>pd", "<cmd>Lazy debug<cr>", { desc = "Lazy | Debug", silent = true })
+
+-- Install
+vim.keymap.set("n", "<leader>pi", "<cmd>Lazy install<cr>", { desc = "Lazy | Install", silent = true })
+
+-- Sync
+vim.keymap.set("n", "<leader>ps", "<cmd>Lazy sync<cr>", { desc = "Lazy | Sync", silent = true })
+
+-- Log
+vim.keymap.set("n", "<leader>pl", "<cmd>Lazy log<cr>", { desc = "Lazy | Log", silent = true })
+
+-- Home
+vim.keymap.set("n", "<leader>ph", "<cmd>Lazy home<cr>", { desc = "Lazy | Home", silent = true })
+
+-- Help
+vim.keymap.set("n", "<leader>pH", "<cmd>Lazy help<cr>", { desc = "Lazy | Help", silent = true })
+
+-- Profile
+vim.keymap.set("n", "<leader>pp", "<cmd>Lazy profile<cr>", { desc = "Lazy | Profile", silent = true })
+
+-- Update
+vim.keymap.set("n", "<leader>pu", "<cmd>Lazy update<cr>", { desc = "Lazy | Update", silent = true })
