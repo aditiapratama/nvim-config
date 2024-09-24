bc91ac6d597ced53d1f36ccbff5c3334fe3ade1f
Aditia A. Pratama <aditia.ap@gmail.com>
2024-09-24 15:28:27 +0800

    enable colorizer

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -2,7 +2,7 @@
 -- NOTE: Preview colors
 return {
   "NvChad/nvim-colorizer.lua",
-  enabled = false,
+  enabled = true,
   opts = {
     user_default_options = {
       RGB = true, -- #RGB hex codes
