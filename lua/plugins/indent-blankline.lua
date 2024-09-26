33e96eaa39ced48313d0e8245a79812c64456ee8
Aditia A. Pratama <aditia.ap@gmail.com>
2024-09-26 23:28:37 +0800

    add blankline setup

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,15 +1,46 @@
 ---@type NvPluginSpec
 -- NOTE: Show Indentlines
+local highlight = {
+  "RainbowRed",
+  "RainbowYellow",
+  "RainbowBlue",
+  "RainbowOrange",
+  "RainbowGreen",
+  "RainbowViolet",
+  "RainbowCyan",
+}
+
+local highlight_bg = {
+  "CursorColumn",
+  "Whitespace",
+}
+
+local hooks = require "ibl.hooks"
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+  vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+  vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+  vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+  vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+  vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+  vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+  vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
+
 return {
   "lukas-reineke/indent-blankline.nvim",
-  enabled = false,
+  main = "ibl",
+  ---@module "ibl"
+  ---@type ibl.config
+  opts = {},
   event = "BufReadPost",
-  opts = {
-    scope = {
-      show_start = false,
+  require("ibl").setup {
+    indent = { highlight = highlight_bg, char="▏" },
+    whitespace = {
+      highlight = highlight_bg,
+      remove_blankline_trail = false,
     },
+    scope = { enabled = false },
   },
-  config = function(_, opts)
-    require("ibl").setup(opts)
-  end,
 }
