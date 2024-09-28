421906660205dee3aba57b006771e77de589b345
Aditia A. Pratama <aditia.ap@gmail.com>
2024-09-28 20:14:23 +0800

    fix indent-blankline to not start on load

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,46 +1,27 @@
----@type NvPluginSpec
--- NOTE: Show Indentlines
-local highlight = {
-  "RainbowRed",
-  "RainbowYellow",
-  "RainbowBlue",
-  "RainbowOrange",
-  "RainbowGreen",
-  "RainbowViolet",
-  "RainbowCyan",
-}
-
 local highlight_bg = {
   "CursorColumn",
   "Whitespace",
 }
 
-local hooks = require "ibl.hooks"
--- create the highlight groups in the highlight setup hook, so they are reset
--- every time the colorscheme changes
-hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-  vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-  vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-  vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-  vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-  vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-  vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-  vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-end)
-
+---@type NvPluginSpec
+-- NOTE: Show Indentlines
 return {
   "lukas-reineke/indent-blankline.nvim",
-  main = "ibl",
-  ---@module "ibl"
-  ---@type ibl.config
-  opts = {},
   event = "BufReadPost",
-  require("ibl").setup {
-    indent = { highlight = highlight_bg, char="▏" },
+  opts = {
+    indent = {
+      highlight = highlight_bg,
+      char = "▏",
+    },
     whitespace = {
       highlight = highlight_bg,
       remove_blankline_trail = false,
     },
-    scope = { enabled = false },
+    scope = {
+      show_start = false,
+    },
   },
+  config = function(_, opts)
+    require("ibl").setup(opts)
+  end,
 }
