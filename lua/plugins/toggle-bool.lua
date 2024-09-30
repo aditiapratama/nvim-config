860e4e694bbedc55935ac268476c62bda9fe1a1f
Aditia A. Pratama <aditia.ap@gmail.com>
2024-09-30 13:56:48 +0800

    add toggle bool plugin

---
+++ lua/plugins/toggle-bool.lua
@@ -0,0 +1,26 @@
+---@type NvPluginSpec
+--- NOTE:: Bool Toggle plugin
+return {
+  "gerazov/toggle-bool.nvim",
+  event = "BufReadPost",
+  opts = {
+    mapping = "<leader>tt",
+    additional_toggles = {
+      Yes = "No",
+      On = "Off",
+      ["0"] = "1",
+      Enable = "Disable",
+      Enabled = "Disabled",
+      First = "Last",
+      Before = "After",
+      Persistent = "Ephemeral",
+      Internal = "External",
+      Ingress = "Egress",
+      Allow = "Deny",
+      All = "None",
+    },
+  },
+  config = function(_, opts)
+    require("toggle-bool").setup(opts)
+  end,
+}
