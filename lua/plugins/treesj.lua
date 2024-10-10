3834ecbacc2adba820105b2cb2d636effeda71eb
Aditia A. Pratama <aditia.ap@gmail.com>
2024-10-10 12:10:55 +0800

    add treesj plugin

---
+++ lua/plugins/treesj.lua
@@ -0,0 +1,11 @@
+--@type NvPluginSpec
+--NOTE: Split lines to avoid the max line lengths
+return {
+  'Wansmer/treesj',
+  event = 'BufReadPost',
+  keys = { '<space>m', '<space>j', '<space>s' },
+  dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter`
+  config = function()
+    require('treesj').setup({--[[ your config ]]})
+  end,
+}
