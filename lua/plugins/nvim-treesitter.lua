74cbf5eeb1736553a4c6c180a9ced3fdbdaf55fc
Aditia A. Pratama <aditia.ap@gmail.com>
2024-10-19 10:51:29 +0800

    update treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -15,11 +15,6 @@ return {
     "TSToggle",
     "TSBufToggle",
   },
-  -- config = function(_, opts)
-  --   dofile(vim.g.base46_cache .. "syntax")
-  --   dofile(vim.g.base46_cache .. "treesitter")
-  --   require("nvim-treesitter.configs").setup(opts)
-  -- end,
   dependencies = {
     {
       "windwp/nvim-ts-autotag",
@@ -76,11 +71,8 @@ return {
       "lua",
       "markdown",
       "markdown_inline",
-      "ninja",
       "python",
       "regex",
-      "rst",
-      "toml",
       "tsx",
       "typescript",
       "vim",
