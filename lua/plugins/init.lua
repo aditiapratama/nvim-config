cccbe395c6200593bc2bdc70a02712ec6c480f1c
Aditia A. Pratama <aditia.ap@gmail.com>
2024-10-21 09:29:15 +0800

    clean up plugins init

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,46 +1,18 @@
 ---@type NvPluginSpec
 -- NOTE: Default Plugins
 return {
-  -- NvChad Default Terminal
   {
     "NvChad/nvterm",
     -- enabled = false,
   },
   {
     "NvChad/ui",
+    config = function()
+      require "nvchad"
+    end,
   },
   { "nvchad/volt" },
   {
     "nvchad/minty",
   },
-  -- {
-  --   "williamboman/mason.nvim",
-  --   opts = {},
-  -- },
-  -- {
-  --   "nvim-treesitter/nvim-treesitter",
-  --   opts = {
-  --     ensure_installed = {
-  --       "vim",
-  --       "lua",
-  --       "vimdoc",
-  --       "html",
-  --       "css",
-  --       "python",
-  --       "toml",
-  --     },
-  --   },
-  -- },
-  -- {
-  --   "nvimtools/none-ls.nvim",
-  --   enabled = false,
-  --   -- lazy=false,
-  --   ft = { "python" },
-  --   dependencies = {
-  --     "nvimtools/none-ls-extras.nvim",
-  --   },
-  --   config = function()
-  --     require "configs.null-ls"
-  --   end,
-  -- },
 }
