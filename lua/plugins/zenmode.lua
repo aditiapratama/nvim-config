e81763af497bcdb11410be73177635a2e20e76bc
Aditia A. Pratama <aditia.ap@gmail.com>
2024-11-04 21:58:15 +0800

    add zen mode plugin

---
+++ lua/plugins/zenmode.lua
@@ -0,0 +1,18 @@
+return {
+  "folke/zen-mode.nvim",
+  cmd = "ZenMode",
+  opts = {
+    window = {
+      width = 100,
+      options = {
+        foldcolumn = "1", -- disable fold column,
+      },
+    },
+    plugins = {
+      gitsigns = true,
+      tmux = true,
+      alacritty = { enabled = false, font = "16" },
+    },
+  },
+  keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
+}
