9fd4afae11e263e22c1f002a7867ef132bb75b8c
Aditia A. Pratama <aditia.ap@gmail.com>
2025-02-03 09:00:05 +0800

    feat(snacks): minor adjustment

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,18 +2,24 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
+  ---@type snacks.Config
   opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
-    bigfile = { enabled = false },
+    bigfile = { enabled = true },
     dashboard = { enabled = false },
     indent = { enabled = true },
-    input = { enabled = true },
+    input = { enabled = true, timeout = 3000 },
     notifier = { enabled = true },
     quickfile = { enabled = true },
     scroll = { enabled = false },
     statuscolumn = { enabled = false },
     words = { enabled = false },
+    styles = {
+      notification = {
+        -- wo = { wrap = true } -- Wrap notifications
+      },
+    },
   },
 }
