c6ac652f20c7527f0fcd7f4d9c2f6174c53e1dae
Aditia A. Pratama <aditia.ap@gmail.com>
2025-02-18 11:13:55 +0800

    refactor(chadrc): update statusline and tabufline

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,9 +1,12 @@
 -- NOTE: NvChad Related Options
 ---@type ChadrcConfig
+-- This file needs to have same structure as nvconfig.lua
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :(
 local M = {}
 
 local highlights = require "configs.highlights"
-local headers = require "configs.headers"
+local headers = require "core.statusline.headers"
 
 local function get_header(default)
   if vim.g.random_header then
@@ -22,8 +25,7 @@ local function get_header(default)
 end
 
 M.ui = {
-  -- transparency = true,
-  telescope = { style = "bordered" }, -- borderless / bordered
+  telescope = { style = "borderless" }, -- borderless / bordered
   cmp = {
     lspkind_text = true,
     style = "default", -- default/flat_light/flat_dark/atom/atom_colored
@@ -32,21 +34,75 @@ M.ui = {
     },
   },
   statusline = {
-    theme = "minimal", -- default/vscode/vscode_colored/minimal
+    theme = "default", -- default/vscode/vscode_colored/minimal
     -- default/round/block/arrow separators work only for default statusline theme
-
     -- round and block will work for minimal theme only
     separator_style = "default",
+    order = {
+      "mode",
+      "file",
+      "git",
+      "command",
+      "%=",
+      -- "escape_status",
+      "lsp_msg",
+      "macro_recording",
+      "diagnostics",
+      "flutter",
+      "python_venv",
+      "auto_format",
+      "lsps",
+      "linters",
+      "formatters",
+      -- "clients",
+      -- "eol_char",
+      "cursor",
+      "cwd",
+      -- "total_lines",
+    },
+    modules = require("core.statusline").modules,
+  },
+
+  tabufline = {
+    enabled = true,
+    lazyload = true,
+    -- order = { "treeOffset", "buffers", "tabs", "btns" },
+    order = { "buffers", "tabs" },
+    modules = require("core.tabufline").modules,
   },
 }
 
 M.nvdash = {
   load_on_startup = true,
   header = get_header "default",
+  buttons = {
+    { txt = "  Find File", cmd = "Telescope find_files" },
+    { txt = "─", no_gap = true, rep = true },
+    {
+      txt = function()
+        local ok, stats = pcall(function()
+          local lazy_stats = require("lazy").stats()
+          return {
+            loaded = lazy_stats.loaded,
+            count = lazy_stats.count,
+            milliseconds = math.floor(lazy_stats.startuptime) .. " ms",
+          }
+        end)
+        if ok then
+          return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. stats.milliseconds
+        else
+          return "󰇸 Cannot load lazy's status"
+        end
+      end,
+      no_gap = true,
+    },
+    { txt = "─", no_gap = true, rep = true },
+  },
 }
 
 M.cheatsheet = { theme = "grid" } -- simple/grid
 
+-- NOTE: NvChad auto recognise from Conform
 M.mason = {
   cmd = true,
   -- Use names from mason.nvim
@@ -60,68 +116,134 @@ M.mason = {
 
     -- Web Development
     "css-lsp",
-    -- "html-lsp",
+    "emmet-language-server",
+    "eslint-lsp",
+    "html-lsp",
+    "js-debug-adapter",
+    "prettier",
+    "tailwindcss-language-server",
     "typescript-language-server",
+    -- "vtsls",
     -- "deno",
     "vue-language-server",
-    "tailwindcss-language-server",
-    "vtsls",
-    -- "emmet_language_server",
-    -- "eslint-lsp",
 
     -- PHP
     -- "intelephense",
 
     -- C/C++
-    -- "clangd",
-    -- "clang-format",
+    "clang-format",
+    "clangd",
+    "codelldb",
+    -- "cpptools",
 
     -- CMake
+    -- "cmake-language-server",
     -- "neocmakelsp",
 
     -- Java
     -- "jdtls",
 
+    -- Json
+    "json-lsp",
+
     -- Yaml
-    -- "yaml-language-server",
+    "yaml-language-server",
+
+    -- Toml
+    "taplo",
+
+    -- Github Action
+    "actionlint",
 
     -- Python
     "basedpyright",
+    "debugpy",
     "ruff",
-    "pyright",
-    "isort",
-    "black",
-    "mypy",
-    "prettier",
+    -- "pyright",
+    -- "isort",
+    -- "black",
+    -- "mypy",
+    -- "prettier",
+
+    -- C#
+    -- "csharpier",
+    -- "omnisharp",
+    -- "omnisharp-mono",
 
     -- Go
     -- "gopls",
 
-    -- C#
-    -- "omnisharp",
-    -- "omnisharp-mono",
+    -- Markdown
+    "doctoc",
+    "markdownlint",
+    -- "marksman",
+    -- "vale_ls",
 
-    -- Extra
-    "selene",
+    -- Shell
+    "bash-language-server",
+    "beautysh",
+    -- "powershell-editor-services",
     "shellcheck",
     "shfmt",
-    -- "marksman",
+
+    -- Docker
+    "docker-compose-language-service",
+    "dockerfile-language-server",
+
+    -- Database
+    "sql-formatter",
+    -- "sqls",
+
+    -- Others
+    -- vim.fn.executable "hyprland" and "hyprls" or nil,
+    "codespell",
+    "selene",
   },
 }
 
 M.lsp = { signature = false }
 
 M.base46 = {
-  transparency = true,
   theme = "catppuccin",
-  theme_toggle = { "chadracula", "catppuccin" },
+  transparency = true,
+  theme_toggle = { "catppuccin", "chadracula" },
   hl_override = highlights.override,
   hl_add = highlights.add,
   integrations = {
-    "notify",
+    "cmp",
     "dap",
+    "defaults",
+    "devicons",
+    "diffview",
+    "lsp",
+    "markview",
+    "mason",
+    "notify",
+    "nvimtree",
+    "rainbowdelimiters",
+    "statusline",
+    "telescope",
+    "todo",
+    "treesitter",
     "trouble",
+    "whichkey",
+    "blankline",
+    "lspsaga", -- override NvChad's background in highlights.lua
   },
 }
+
+M.colorify = {
+  enabled = true,
+  mode = "virtual", -- fg, bg, virtual
+  virt_text = "󱓻 ",
+  highlight = { hex = true, lspvars = true },
+}
+
+-- M.lazy_nvim = require "core.lazy" -- config for lazy.nvim startup options
+
+-- M.plugins = "plugins"
+
+-- check core.mappings for table structure
+-- M.mappings = require "mappings"
 
 return M
