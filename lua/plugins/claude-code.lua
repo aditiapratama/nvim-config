60fd19d8404c64d61588e5c48b0d7c29f573f603
Aditia A. Pratama <aditia.ap@gmail.com>
2025-03-04 11:48:56 +0800

    feat(AI): add claude code plugins

---
+++ lua/plugins/claude-code.lua
@@ -0,0 +1,14 @@
+return {
+  "greggh/claude-code.nvim",
+  lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim", -- Required for git operations
+  },
+  config = function()
+    require("claude-code").setup {
+      window = {
+        position = "topleft",
+      },
+    }
+  end,
+}
