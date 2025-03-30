ae4ca7a66a981ed452eb7e3664bd9907660353ff
Aditia A. Pratama <aditia.ap@gmail.com>
2025-03-30 08:04:43 +0800

    feat(init): add some default plugins in init

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,9 +1,10 @@
 ---@type NvPluginSpec
 -- NOTE: Default Plugins
 return {
+  -- NvChad Default Terminal
   {
     "NvChad/nvterm",
-    -- enabled = false,
+    enabled = false,
   },
   {
     "NvChad/ui",
@@ -17,9 +18,35 @@ return {
     build = function()
       require("base46").load_all_highlights()
     end,
+    init = function()
+      vim.api.nvim_create_user_command("NvChadBase46LoadAllHighLights", function()
+        require("base46").load_all_highlights()
+      end, { desc = "NvChad | Base46 Load All Highlights" })
+    end,
   },
-  { "nvchad/volt" },
   {
-    "nvchad/minty",
+    "nvzone/minty",
+    dependencies = {
+      "nvzone/volt",
+      cmd = { "Shades", "Huefy" },
+      init = function()
+        vim.keymap.set("n", "<leader>uh", "<cmd>Huefy<cr>", { desc = "Minty | Huefy", silent = true })
+        vim.keymap.set("n", "<leader>us", "<cmd>Shades<cr>", { desc = "Minty | Shades", silent = true })
+      end,
+    },
+    {
+      "nvzone/menu",
+      dependencies = { "nvzone/volt" },
+      enabled = false,
+      event = "VeryLazy",
+    },
+    {
+      "nvzone/showkeys",
+      dependencies = { "nvzone/volt" },
+      cmd = "ShowkeysToggle",
+      init = function()
+        vim.keymap.set("n", "<leader>ok", "<cmd>ShowkeysToggle<cr>", { desc = "Showkeys | Toggle", silent = true })
+      end,
+    },
   },
 }
