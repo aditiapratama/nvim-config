4ee7c3e328df0281cd6b0b40a4b700f090e3e376
Aditia A. Pratama <aditia.ap@gmail.com>
2025-04-18 08:40:19 +0800

    feat(incline): add git repo name if available

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -9,6 +9,43 @@ local function get_venv(variable)
   end
   return venv
 end
+-- Function to get git repository information
+local function get_git_info()
+  -- Get the git directory
+  local git_dir = vim.fn.system("git rev-parse --git-dir 2>/dev/null"):gsub("\n", "")
+  if git_dir == "" or git_dir:match "fatal" then
+    return { is_git = false }
+  end
+
+  -- Get the repository name (last part of the remote URL)
+  local remote_url = vim.fn.system("git config --get remote.origin.url 2>/dev/null"):gsub("\n", "")
+  if remote_url == "" then
+    return { is_git = true, provider = "git", repo_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t") }
+  end
+
+  local repo_name = ""
+  local provider = "git"
+
+  -- Extract repo name and identify provider
+  if remote_url:match "github%.com" then
+    provider = "github"
+    repo_name = remote_url:match "github%.com[:/](.+)%.git$" or remote_url:match "github%.com[:/](.+)$"
+  elseif remote_url:match "gitlab%.com" then
+    provider = "gitlab"
+    repo_name = remote_url:match "gitlab%.com[:/](.+)%.git$" or remote_url:match "gitlab%.com[:/](.+)$"
+  elseif remote_url:match "bitbucket%.org" then
+    provider = "bitbucket"
+    repo_name = remote_url:match "bitbucket%.org[:/](.+)%.git$" or remote_url:match "bitbucket%.org[:/](.+)$"
+  else
+    -- Extract repo name from generic git URL
+    repo_name = remote_url:match "[:/]([^/]+/[^/]+)%.git$" or remote_url:match "[:/]([^/]+/[^/]+)$"
+    if not repo_name then
+      repo_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+    end
+  end
+
+  return { is_git = true, provider = provider, repo_name = repo_name }
+end
 
 ---@type NvPluginSpec
 --- NOTE: Incline for naming if modified
@@ -41,18 +78,46 @@ return {
 
         local venv = get_venv "CONDA_DEFAULT_ENV" or get_venv "VIRTUAL_ENV" or " "
 
+        -- Get git repository information
+        local git_info = get_git_info()
+
         -- Get icon and color for filename
         local file_icon, file_color = require("nvim-web-devicons").get_icon_color(filename)
         -- Use a custom folder icon if devicons can't provide one
         local folder_icon = " " -- Default folder icon (nerd font)
         local folder_color = colors.green --"#6d8086" -- Default folder color
 
+        -- Set git provider icon
+        local git_icon = "   " -- Default git icon
+        local git_color = colors.orange
+
+        if git_info.is_git then
+          if git_info.provider == "github" then
+            git_icon = "  " -- GitHub icon
+            git_color = colors.blue
+          elseif git_info.provider == "gitlab" then
+            git_icon = " 󰮠  " -- GitLab icon
+            git_color = colors.orange
+          elseif git_info.provider == "bitbucket" then
+            git_icon = "  " -- Bitbucket icon
+            git_color = colors.cyan
+          end
+        end
+
         local sep = ""
         local render_venv = function()
           if venv == " " then
             return " "
           else
-            return "   " .. venv
+            return "  " .. venv
+          end
+        end
+
+        local render_git_info = function()
+          if not git_info.is_git then
+            return ""
+          else
+            return "" .. git_icon .. "" .. git_info.repo_name
           end
         end
 
@@ -64,15 +129,16 @@ return {
           { " " },
           { file_icon, guifg = file_color },
           { " " },
-          { " " },
+          -- { " " },
           { filename },
           -- { " " },
           { " " },
           { " " },
           { folder_icon, guifg = folder_color },
-          { " " },
+          -- { " " },
           { parent_dir },
           { render_venv() },
+          { render_git_info(), guifg = git_color },
           -- { "" .. sep .. " " },
           guibg = bgcol,
         }
