f4233b4d8c6d2ca219551b9cfb611f607bcb5e73
Aditia A. Pratama <aditia.ap@gmail.com>
2025-04-21 12:50:11 +0800

    fix(split): disable tree split plugin

--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -2,6 +2,7 @@
 --NOTE: Split lines to avoid the max line lengths
 return {
   "Wansmer/treesj",
+  enabled = false,
   event = { "BufReadPost", "BufNewFile" },
   keys = { "<space>m", "<space>j", "<space>s" },
   dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
