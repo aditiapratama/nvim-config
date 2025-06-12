1523a13f4af48883f2e1c1c644ec11abc9136853
Aditia A. Pratama <aditia.ap@gmail.com>
2025-06-12 11:50:13 +0800

    feat(nvchad): add right click menu for nvchad nvimtree

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,6 @@
 ---@type NvPluginSpec
 -- NOTE: Default Plugins
 return {
-  -- NvChad Default Terminal
   {
     "NvChad/nvterm",
     enabled = false,
@@ -18,35 +17,44 @@ return {
     build = function()
       require("base46").load_all_highlights()
     end,
+  },
+  {
+    "nvzone/menu",
     init = function()
-      vim.api.nvim_create_user_command("NvChadBase46LoadAllHighLights", function()
-        require("base46").load_all_highlights()
-      end, { desc = "NvChad | Base46 Load All Highlights" })
+      vim.keymap.set("n", "<C-t>", function()
+        require("menu").open "default"
+      end, {})
+
+      -- mouse users + nvimtree users!
+      vim.keymap.set({ "n", "v" }, "<RightMouse>", function()
+        require("menu.utils").delete_old_menus()
+
+        vim.cmd.exec '"normal! \\<RightMouse>"'
+
+        -- clicked buf
+        local buf = vim.api.nvim_win_get_buf(vim.fn.getmousepos().winid)
+        local options = vim.bo[buf].ft == "NvimTree" and "nvimtree" or "default"
+
+        require("menu").open(options, { mouse = true })
+      end, {})
     end,
   },
   {
-    "nvzone/minty",
-    dependencies = {
-      "nvzone/volt",
-      cmd = { "Shades", "Huefy" },
-      init = function()
-        vim.keymap.set("n", "<leader>uh", "<cmd>Huefy<cr>", { desc = "Minty | Huefy", silent = true })
-        vim.keymap.set("n", "<leader>us", "<cmd>Shades<cr>", { desc = "Minty | Shades", silent = true })
-      end,
-    },
-    {
-      "nvzone/menu",
-      dependencies = { "nvzone/volt" },
-      enabled = false,
-      event = "VeryLazy",
-    },
-    {
-      "nvzone/showkeys",
-      dependencies = { "nvzone/volt" },
-      cmd = "ShowkeysToggle",
-      init = function()
-        vim.keymap.set("n", "<leader>ok", "<cmd>ShowkeysToggle<cr>", { desc = "Showkeys | Toggle", silent = true })
-      end,
-    },
+    "nvzone/showkeys",
+    init = function()
+      vim.keymap.set(
+        "n",
+        "<leader>ok",
+        "<cmd>ShowkeysToggle<cr>",
+        { desc = "Screenkey | Toggle Screenkey", silent = true }
+      )
+    end,
+    cmd = "ShowkeysToggle",
+  },
+  {
+    "nvzone/typr",
+    dependencies = "nvzone/volt",
+    opts = {},
+    cmd = { "Typr", "TyprStats" },
   },
 }
