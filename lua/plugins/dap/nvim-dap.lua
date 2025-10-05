b93dc52135e05cea8e4e3006500d06c9883f6be9
Aditia A. Pratama <aditia.ap@gmail.com>
2025-10-05 11:24:38 +0800

    add godot

--- lua/plugins/dap/nvim-dap.lua
+++ lua/plugins/dap/nvim-dap.lua
@@ -2,16 +2,17 @@
 -- NOTE: Debug Adapter Protocol
 return {
   "mfussenegger/nvim-dap",
+  -- enabled = false,
   config = function()
     -- NOTE: Check out this for guide
     -- https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation
     local dap = require "dap"
     vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
 
-    -- local dapui = require "dapui"
-    -- dap.listeners.after.event_initialized["dapui_config"] = function()
-    --   dapui.open()
-    -- end
+    local dapui = require "dapui"
+    dap.listeners.after.event_initialized["dapui_config"] = function()
+      dapui.open()
+    end
 
     -- local path = require("mason-registry").get_package("debugpy"):get_install_path()
     -- if vim.loop.os_uname().sysname:find "Windows" then
@@ -30,6 +31,7 @@ return {
 
     -- NOTE: Make sure to install the needed files/exectubles through mason
     -- require "plugins.dap.settings.godot"
+    require "plugins.dap.settings.godot"
     require "plugins.dap.settings.debugpy"
   end,
 }
